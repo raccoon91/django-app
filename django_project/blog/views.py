fd10532b6e77caf1b643dd4e30ccdc3f132e1725
raccoon91 <qsx314@gmail.com>
2021-02-01 17:37:56 +0900

    add blog templates width bootstrap

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        "author": "raccoon",
+        "title": "blog post 1",
+        "content": "first post content",
+        "date_posted": "August 27, 2018",
+    },
+    {
+        "author": "raccoon",
+        "title": "blog post 2",
+        "content": "second post content",
+        "date_posted": "August 27, 2018",
+    },
+    {
+        "author": "raccoon",
+        "title": "blog post 3",
+        "content": "third post content",
+        "date_posted": "August 27, 2018",
+    },
+]
 
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, "blog/about.html", {"title": "About"})
