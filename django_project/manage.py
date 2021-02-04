fe21c90b45c5d40a61fde02dc480e77724a7377b
raccoon91 <qsx314@gmail.com>
2021-02-04 03:11:49 +0000

    add user reset password

--- django_project/manage.py
+++ django_project/manage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+import dotenv
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
+if __name__ == "__main__":
+    dotenv.read_dotenv()
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
